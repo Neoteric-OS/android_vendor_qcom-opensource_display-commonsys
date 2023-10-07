9a7387b069ffff56801fe0ce47e91905abe7191b
Jake Weinstein <jake@aospa.co>
2023-10-07 14:50:04 +0900

    display: Make UBWC-P opt-out
    Enabling by default for new targets. Disable for legacy targets.
    This avoids needing to keep defining flags going forward
    and means we can drop these patches when existing devices
    age out.
    
    Change-Id: I2cbfa3fe6a9f1c5522286d27aab713027e743bc9

--- config/display-product-commonsys.mk
+++ config/display-product-commonsys.mk
@@ -8,7 +8,7 @@ PRODUCT_PACKAGES += libdisplayconfig.system \
 
 SOONG_CONFIG_NAMESPACES += qtidisplaycommonsys
 # Soong Keys
-SOONG_CONFIG_qtidisplaycommonsys := displayextension composer3ext qticomposerversion gralloc_handle_has_no_reserved_size gralloc_handle_has_no_custom_content_md_reserved_size
+SOONG_CONFIG_qtidisplaycommonsys := displayextension composer3ext qticomposerversion gralloc_handle_has_no_reserved_size gralloc_handle_has_no_custom_content_md_reserved_size gralloc_handle_has_no_ubwcp
 # Soong Values
 
 # displayextension controls global compile time disablement of SF extensions
@@ -69,4 +69,10 @@ SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_custom_content_md_reserve
 
 ifeq ($(TARGET_GRALLOC_HANDLE_HAS_NO_CUSTOM_CONTENT_MD_RESERVED_SIZE),true)
     SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_custom_content_md_reserved_size := true
+endif
+
+SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_ubwcp := false
+
+ifeq ($(TARGET_GRALLOC_HANDLE_HAS_NO_UBWCP),true)
+    SOONG_CONFIG_qtidisplaycommonsys_gralloc_handle_has_no_ubwcp := true
 endif
