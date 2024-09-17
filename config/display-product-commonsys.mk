b59bddb4278fd8a7c824d4f69724c05b20a1f894
Rheygine Medel <quic_rmedel@quicinc.com>
2024-09-17 15:46:09 -0700

    config: Add new soong variable for HWC version in system
    This change adds a new soong variable, qticomposerversion, that
    determines the version of composer3 in the system.
    
    Change-Id: I3eada71938aad08a483327ffd5bcb3c18ca61c48
    CRs-Fixed: 3926636

--- config/display-product-commonsys.mk
+++ config/display-product-commonsys.mk
@@ -8,7 +8,7 @@ PRODUCT_PACKAGES += libdisplayconfig.system \
 
 SOONG_CONFIG_NAMESPACES += qtidisplaycommonsys
 # Soong Keys
-SOONG_CONFIG_qtidisplaycommonsys := displayextension composer3ext
+SOONG_CONFIG_qtidisplaycommonsys := displayextension composer3ext qticomposerversion
 # Soong Values
 
 # displayextension controls global compile time disablement of SF extensions
@@ -21,7 +21,25 @@ SOONG_CONFIG_qtidisplaycommonsys_displayextension := false
 # properties
 SOONG_CONFIG_qtidisplaycommonsys_composer3ext := false
 
+
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     SOONG_CONFIG_qtidisplaycommonsys_displayextension := true
     SOONG_CONFIG_qtidisplaycommonsys_composer3ext := true
+endif
+
+# Enable conditional compilation for HWC's version in the system image.
+# Properly determine the system image version to select the appropriate version for the internal
+# composer's interface. Two key build properties are PLATFORM_VERSION_CODENAME and PLATFORM_VERSION.
+# PLATFORM_VERSION_CODENAME holds the string codename of the current Android version.
+# PLATFORM_VERSION contains the version number of the current Android version, which matches
+# PLATFORM_VERSION_CODENAME until the FRC stage.
+SOONG_CONFIG_qtidisplaycommonsys_qticomposerversion := composer3_v3
+
+# TODO: Update VanillaIceCream to Android W's code name
+ifeq ($(PLATFORM_VERSION_CODENAME), VanillaIceCream)
+    ifeq ($(PLATFORM_VERSION), $(filter $(PLATFORM_VERSION), VanillaIceCream 16))
+        SOONG_CONFIG_qtidisplaycommonsys_qticomposerversion := composer3_v4
+    else
+        SOONG_CONFIG_qtidisplaycommonsys_qticomposerversion := composer3_v3
+    endif
 endif
